17cd12fd7f7d44071f89a82e1f94d3e5855db7ea
Tushar06more <tusharrmore06@gmail.com>
2022-06-19 10:37:29 +0530

    PBA-2: Create Service Layer

--- src/main/java/com/PhoneBook/repository/ContactRepository.java
+++ src/main/java/com/PhoneBook/repository/ContactRepository.java
@@ -1,5 +1,12 @@
 package com.PhoneBook.repository;
 
-public class ContactRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
+import org.springframework.stereotype.Repository;
+
+import com.PhoneBook.model.Contact;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer>{
 
 }
